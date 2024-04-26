05099674595dd50dcc3132df927f4d3158a3a486
Saleem Ayoub Khan <75361545+saleem189@users.noreply.github.com>
2024-04-26 09:32:37 +0500

    Create ShipmentStatusStage.php

---
+++ ShipmentStatusStage.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Services\Companies\Shipments\Enums;
+
+use App\Common\Enum\Enum;
+use App\Services\Companies\Shipments\ShipmentStatus; // Assuming this is the correct namespace for ShipmentStatus
+
+class ShipmentStatusStage extends Enum
+{
+    const PENDING = [
+        ShipmentStatus::QUOTE_ACTIVE,
+    ];
+
+    const ACTIVE = [
+        ShipmentStatus::TENDER_ACCEPTED,
+        ShipmentStatus::DISPATCHED,
+        ShipmentStatus::AT_PICKUP,
+        ShipmentStatus::EN_ROUTE,
+        ShipmentStatus::AT_DELIVERY,
+        ShipmentStatus::ROUTE_COMPLETE,
+    ];
+
+    const COMPLETED = [
+        ShipmentStatus::DELIVERED,
+        ShipmentStatus::COMPLETED,
+        ShipmentStatus::CANCELED,
+    ];
+}
